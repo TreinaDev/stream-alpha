72acb95dfeb0025e0ac59c7ec25241f0b3cb2cf7
Otávio Lins <otavioslins97@gmail.com>
2021-11-17 15:18:24 -0300

    Rubocop

--- spec/requests/streamer_authentication_spec.rb
+++ spec/requests/streamer_authentication_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 describe 'streamer autheticated' do
   context 'cannot' do
-    it "register video without login" do
+    it 'register video without login' do
       get new_video_path
-      
+
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(new_streamer_session_path)
     end
 
     it 'create video without login' do
       post videos_path
-      
+
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(new_streamer_session_path)
     end
