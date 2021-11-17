ef4359df3855958e60e335902e600ef0cc2643c8
Luiz Fernando <luizfegomez@gmail.com>
2021-11-17 16:17:48 -0300

    Adicionando testes ao perfil do streamer

--- spec/system/streamer_register_profile_spec.rb
+++ spec/system/streamer_register_profile_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'Streamer register profile' do
-  context 'login' do
+describe 'Streamer log in' do
+  context 'and register profile' do
     it 'and fill profile' do
-      jogador = create(:streamer)
+      streamer = create(:streamer)
 
       visit root_path
       click_on 'Entrar como Streamer'
-      fill_in 'Email', with: jogador.email
-      fill_in 'Senha', with: jogador.password
+      fill_in 'Email', with: streamer.email
+      fill_in 'Senha', with: streamer.password
       click_on 'Entrar'
       fill_in 'Nome', with: 'Fulano'
       fill_in 'Descrição', with: 'Jovem de 22 anos sem ter o que fazer da vida e fica fazendo live na internet'
@@ -17,7 +17,7 @@ describe 'Streamer register profile' do
       fill_in 'URL do Twitter', with: 'https://www.instagram.com/fulano/'
       click_on 'Criar Perfil de Streamer'
 
-      expect(current_path).to eq streamer_profile_path(jogador.streamer_profile.id)
+      expect(current_path).to eq streamer_profile_path(streamer.streamer_profile.id)
       expect(page).to have_content 'Perfil de Streamer criado com sucesso!'
       expect(page).to have_content 'Descrição Jovem de 22 anos sem ter o que fazer da vida ' \
                                    'e fica fazendo live na internet'
@@ -27,6 +27,42 @@ describe 'Streamer register profile' do
       expect(page).to have_link 'https://www.facebook.com/fulano/'
       expect(page).to have_link 'https://twitter.com/fulano/'
       expect(page).to have_link 'https://www.instagram.com/fulano/'
+    end
+
+    it 'and fill wrong' do
+      streamer = create(:streamer)
+
+      visit root_path
+      click_on 'Entrar como Streamer'
+      fill_in 'Email', with: streamer.email
+      fill_in 'Senha', with: streamer.password
+      click_on 'Entrar'
+      fill_in 'Nome', with: ''
+      fill_in 'Descrição', with: ''
+      fill_in 'URL do Facebook', with: 'https://www.facebook.com/fulano/'
+      fill_in 'URL do Instagram', with: 'https://twitter.com/fulano/'
+      fill_in 'URL do Twitter', with: 'https://www.instagram.com/fulano/'
+      click_on 'Criar Perfil de Streamer'
+
+      expect(current_path).to eq streamer_profiles_path
+      expect(page).to have_content('Nome não pode ficar em branco')
+      expect(page).to have_content('Descrição não pode ficar em branco')
+    end
+  end
+  context 'and profile already exists' do
+    it 'and is redirected to home page' do
+      streamer = create(:streamer)
+      streamer_profile = create(:streamer_profile, streamer: streamer)
+      
+      visit root_path
+      click_on 'Entrar como Streamer'
+      fill_in 'Email', with: streamer.email
+      fill_in 'Senha', with: streamer.password
+      click_on 'Entrar'
+     
+
+      expect(current_path).to eq root_path
+      expect(page).to have_link('Meu Perfil', href: streamer_profile_path(streamer.streamer_profile.id))
     end
   end
 end
