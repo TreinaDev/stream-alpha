fc9770b962d5fa6375fe39a6aad082292186fd26
Plínio Larrubia <plinio2xd@gmail.com>
2021-11-20 03:14:48 -0300

    Remover configuração duplicada do Active Storage

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  config.active_storage.service = :test
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
