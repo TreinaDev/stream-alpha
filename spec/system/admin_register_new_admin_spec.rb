50fee5559843b2a7dc0214363df5dae021b564a9
Luiz Fernando <luizfegomez@gmail.com>
2021-11-25 13:44:27 -0300

    Email de admin precisa ser do domínio @gamestream.com.br

--- spec/system/admin_register_new_admin_spec.rb
+++ spec/system/admin_register_new_admin_spec.rb
@@ -26,5 +26,21 @@ describe 'Some' do
 
       expect(page).to have_content('Administrador cadastrado com sucesso!')
     end
+
+    it 'register a new admin but fills wrong' do
+      admin = create(:admin)
+      login_as admin, scope: :admin
+
+      visit root_path
+      click_on 'Área do administrador'
+      click_on 'Cadastrar novo Administrador'
+      fill_in 'Email', with: ''
+      fill_in 'Senha', with: ''
+      click_on 'Cadastrar'
+
+      expect(page).to have_content('Email não pode ficar em branco')
+      expect(page).to have_content('deve pertencer ao domínio @gamestream.com.br')
+      expect(page).to have_content('Senha não pode ficar em branco')
+    end
   end
 end
