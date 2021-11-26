cd0ca58ba021fa6eb786928fdac11fe116f68dc4
DaniloJNS <dan.silva.13.zx@gmail.com>
2021-11-25 21:54:43 -0300

    nav links

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -1,12 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button", "list"];
+  static targets = ["button1", "button2", "drop1", "drop2"];
   connect() {
     if (this.hasListTarget) {
     }
   }
   toggle() {
-    this.listTarget.classList.remove("hidden");
+    console.log(this.drop1Target.classList);
+    if (this.drop1Target.classList.contains("hidden")) {
+      this.drop1Target.classList.remove("hidden");
+    } else {
+      this.drop1Target.classList.add("hidden");
+    }
+    this.drop2Target.classList.add("hidden");
+  }
+  toggle_drop() {
+    if (this.drop2Target.classList.contains("hidden")) {
+      this.drop2Target.classList.remove("hidden");
+    } else {
+      this.drop2Target.classList.add("hidden");
+    }
+    this.drop1Target.classList.add("hidden");
   }
 }
